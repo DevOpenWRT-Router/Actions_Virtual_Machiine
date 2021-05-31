c4fca2226c4f4d13da77d11d18062e0bc65b6f89
Eliminater74 <eliminater74@gmail.com>
2021-05-31 19:31:20 -0400

    add ubuntu apt commands to update and create openwrt envirement

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -4,7 +4,13 @@
 
 # Setting the time zone
 #sudo timedatectl set-timezone "Asia/Shanghai"
+sudo timedatectl set-timezone "America/New_York"
 
 # Install the tools you need to use
-#sudo apt update
-#sudo apt install -y neofetch
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential ccache ecj fastjar file g++ gawk \
+gettext git java-propose-classpath libelf-dev libncurses5-dev \
+libncursesw5-dev libssl-dev python python2.7-dev python3 unzip wget \
+python3-distutils python3-setuptools rsync subversion swig time \
+xsltproc zlib1g-dev 
